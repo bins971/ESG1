d92795d2cad2eadd5636fbf6557704e8516664dc
bins971 <kielvincentalpapara003@gmail.com>
2024-10-21 03:43:03 -0400

    change project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-cyan-700 text-2xl font-bold">Good morning.</p>
+    </>
+  )
+}
+
+export default App
